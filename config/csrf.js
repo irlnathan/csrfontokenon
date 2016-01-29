cd89fba3a288725124bcce2b1e4485e3a8e1aa30
Irl Nathan <irlnathan@gmail.com>
2016-01-29 11:35:36 -0600

    config

--- config/csrf.js
+++ config/csrf.js
@@ -60,5 +60,5 @@ module.exports.csrf = true;
 
 module.exports.csrf = {
    grantTokenViaAjax: true,
-   origin: 'https://badsite.herokuapp.com'
+   origin: 'https://badsite.herokuapp.com', 'badsite.herokuapp.com'
 }
